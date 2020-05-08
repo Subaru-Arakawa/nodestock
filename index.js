e3c9ff0d9f2f3f2d58e77f6ebc26e4369c7cbb21
subaru arakawa <subaru@takepartsfactory.com>
2020-05-08 09:56:54 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+var exphbs  = require('express-handlebars');
+const path = require('path')
+
+const PORT = process.env.PORT || 5000;
+
+//Set Handlebar Mid
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+const otherstuff = "hello there, this is other stff"
+
+//Set Handlbar routew
+app.get('/', function (req, res) {
+    res.render('home', {
+        stuff: otherstuff
+    });
+});
+
+//sET static folder
+
+app.use(express.static(path.join(__dirname,'public')));
+
+
+app.listen(PORT,( ) => {
+    console.log('Server Listeing on port ' + PORT);
+    
+});
